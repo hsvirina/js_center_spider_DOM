91c5009fbcc116eb401824261bc2308a481d65d3
Hanna Svirina <26anna07@ukr.net>
2024-06-17 14:45:26 +0200

    make build

---
+++ index.600fc22f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall");e.style.display="flex",e.style.alignItems="center",e.style.justifyContent="center",document.querySelector(".spider").style.position="relative";
+//# sourceMappingURL=index.600fc22f.js.map
